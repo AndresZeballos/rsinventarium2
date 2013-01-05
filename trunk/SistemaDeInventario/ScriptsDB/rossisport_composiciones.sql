2a34444a453e164f48966c4b61d49ba06d00cc9c
andreszeballosjuanico@gmail.com <andreszeballosjuanico@gmail.com@da9e5ad1-38e0-dff8-54d3-acc91cd0f96b>
2013-01-05 22:21:31 +0000

    Scripts de creacion de tablas. Se agregan los scripts para mysql de creacion de las tablas del sistema. Luego se agregara un instructivo con el orden en que se deben ejecutar (para evitar los errores de clave foranea inexistente). Ademas se eliminaron los archivos innecesarios.

---
+++ trunk/SistemaDeInventario/ScriptsDB/rossisport_composiciones.sql
@@ -0,0 +1,49 @@
+CREATE DATABASE  IF NOT EXISTS `rossisport` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `rossisport`;
+-- MySQL dump 10.13  Distrib 5.5.16, for Win32 (x86)
+--
+-- Host: localhost    Database: rossisport
+-- ------------------------------------------------------
+-- Server version	5.5.25a
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `composiciones`
+--
+
+DROP TABLE IF EXISTS `composiciones`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `composiciones` (
+  `codigo` varchar(100) NOT NULL,
+  `component` varchar(100) NOT NULL,
+  `porcentaje` int(11) NOT NULL,
+  PRIMARY KEY (`codigo`,`component`),
+  KEY `codigo_idx` (`codigo`),
+  KEY `composicion_idx` (`component`),
+  KEY `component_idx` (`component`),
+  CONSTRAINT `codigo` FOREIGN KEY (`codigo`) REFERENCES `descripciones` (`codigo`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `component` FOREIGN KEY (`component`) REFERENCES `componentes` (`component`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2013-01-05 20:13:29
